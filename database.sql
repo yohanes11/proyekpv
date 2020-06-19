ec558ef746e7e00658c53379c06544c9d83baaa1
yohanes11 <66712826+yohanes11@users.noreply.github.com>
2020-06-19 22:29:36 +0700

    database.txt

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[score] (
+    [nama]       VARCHAR (50) NULL,
+    [dif]        VARCHAR (50) NULL,
+    [difficulty] VARCHAR (50) NULL,
+    [level]      VARCHAR (50) NULL,
+    [menit]      VARCHAR (50) NULL,
+    [detik]      VARCHAR (50) NULL
+);
+
